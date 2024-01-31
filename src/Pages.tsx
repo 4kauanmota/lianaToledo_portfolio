01141cd58450cc5b087e64725bcd35fe80add362
Kauan Soares Mota <4kauanmota@gmail.com>
2024-01-31 19:40:50 -0300

    :sparkles: feat: Global top navigation bar

--- src/Pages.tsx
+++ src/Pages.tsx
@@ -7,11 +7,16 @@ import Projects from "./pages/Projects";
 import AboutMe from "./pages/AboutMe";
 
 import Error404 from "./pages/Error404";
+import TopNavigationBar from "./components/atoms/TopNavigationBar";
 
 const Pages = () => {
   return (
     <>
       <Router>
+        <div style={{ height: "11%" }}>
+          <TopNavigationBar />
+        </div>
+
         <Routes>
           <Route path="/">
             <Route index={true} element={<AboutMe />} />
---
+++ src/components/atoms/TopNavigationBar.tsx
@@ -0,0 +1,94 @@
+import React, { useEffect, useRef, useState } from "react";
+import { Link } from "react-router-dom";
+
+import styles from "./TopNavigationBar.module.scss";
+
+const TopNavigationBar = () => {
+  const [url, setUrl] = useState(null);
+
+  const menu = useRef(null);
+  const aboutMe = useRef(null);
+  const illustrations = useRef(null);
+  const projects = useRef(null);
+
+  useEffect(() => {
+    const currentUrl = location.pathname;
+    setUrl(currentUrl);
+
+    aboutMe.current.classList.remove(styles.selected);
+    illustrations.current.classList.remove(styles.selected);
+    projects.current.classList.remove(styles.selected);
+
+    if (url === "/") {
+      aboutMe.current.classList.add(styles.selected);
+    } else if (url === "/illustrations") {
+      illustrations.current.classList.add(styles.selected);
+    } else if (url === "/projects") {
+      projects.current.classList.add(styles.selected);
+    }
+  }, [url]);
+
+  const select = (event: React.MouseEvent<HTMLLIElement>) => {
+    setUrl(null);
+
+    const element = event.target as HTMLElement;
+    element.classList.add(styles.selected);
+  };
+
+  const unSelect = (event: React.MouseEvent<HTMLLIElement>) => {
+    setUrl(null);
+
+    const element = event.target as HTMLElement;
+    element.classList.remove(styles.selected);
+  };
+
+  const menuInteraction = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const element = event.currentTarget as HTMLElement;
+
+    element.classList.contains(styles.open)
+      ? element.classList.remove(styles.open)
+      : element.classList.add(styles.open);
+  };
+
+  return (
+    <nav id={styles.menu}>
+      <div>
+        <Link to={"/"}>
+          <span onClick={() => setUrl(null)}>
+            <h1>Liana Toledo</h1>
+          </span>
+        </Link>
+
+        <button
+          ref={menu}
+          className={styles.hamburguerMenu}
+          onClick={menuInteraction}
+        >
+          <span></span>
+        </button>
+
+        <ul>
+          <li onMouseOver={select} onMouseOut={unSelect} onClick={select}>
+            <Link ref={aboutMe} to={"/"}>
+              About me
+            </Link>
+          </li>
+
+          <li onMouseOver={select} onMouseOut={unSelect} onClick={select}>
+            <Link ref={illustrations} to={"/illustrations"}>
+              Illustrations
+            </Link>
+          </li>
+
+          <li onMouseOver={select} onMouseOut={unSelect} onClick={select}>
+            <Link ref={projects} to={"/projects"}>
+              Projects
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default TopNavigationBar;
