8c7b8ab221200476bc0c544884ab1bfacb7d253b
Kauan Soares Mota <4kauanmota@gmail.com>
2024-03-20 23:10:17 -0300

    :sparkles: feat: New image source

--- src/pages/Illustrations.tsx
+++ src/pages/Illustrations.tsx
@@ -16,152 +16,152 @@ const Illustrations = () => {
           <IllustrationsOrganization>
             <Illustration
               date="Feb. 2024"
-              image="https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYS_woTtzm_m4mYVFkOziUdAkUnNIXkWcWW55LxoMOYa8njI-oKfMdohEvW00G6dIo1cs0vYTcIUj0OpAo1GJ1Nlilsj7Q"
+              image="https://i.postimg.cc/2yf9fHD3/12-fev-2024.png"
             />
 
             <Illustration
               date="Jan. 2024"
-              image="https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYTHYAZh-G84jlCuF2oeCKqLUKZYFE_9eGcmIF6qUGMVKEzIs6A6wwg1qGJnuYu4XYBWPXzsdnPOuN_N4EmIxJLXhXiQ"
+              image="https://i.postimg.cc/qvmFCX8Z/11-jan-2024.png"
             />
 
             <Illustration
               date="Jan. 2024"
-              image="https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYTIqEChI29hj-XK1TsIsaAtc5xQt-xevmqURZ7Qs8lteu7l_M09plxYRjVDJeZ4hNlZMi1u7ethpS0_2kspd8vkphogTw"
+              image="https://i.postimg.cc/d1qxxbJF/14-jan-2024.png"
             />
 
             <Illustration
               date="Jan. 2024"
-              image="https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYTUSAobQDRiCR_tLevNyxw_wEwufazRL57_FKqiOmmGocfJikPe5lg0q3wA7W_2QS7MlROMHkMyXWSwTUJCStz6f0aOow"
+              image="https://i.postimg.cc/529G0kFC/2-jan-2024.png"
             />
 
             <Illustration
               date="Dec. 2023"
-              image="https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYT9Lk9Yzwl2BGDrv_NMPSP_CHo0ykaUci8Cra5kxHRpaUwXkvzE5Us6E1jHp4WTspTtCDgopXF4PGBZApRlP55f7nVqFg"
+              image="https://i.postimg.cc/76ztcFjp/13-dec-2023.png"
             />
 
             <Illustration
               date="Dec. 2023"
-              image="https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYTOtxgWOQ4zz0LynOXD19oczepFRaa3gOUhfpESF141Cwe2jvsK628Gw3vzw_1OuOw4MEml-nTCoJiqp9_jr-nE1O_V"
+              image="https://i.postimg.cc/HLRP6Yvw/13-dez-2023.png"
             />
 
             <Illustration
               date="Dec. 2023"
-              image="https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYRvfFlf8-CA3NgQ3l3FOXsGi6fHDwiBlGhqihW1Bn2KwoKLSoDAn5OejrcEta2SvarsjKWgzDI9b0EmpOVV3ZgRcIn2iA"
+              image="https://i.postimg.cc/vmGhY5kL/4-dezembro-2023.jpg"
             />
 
             <Illustration
               date="Nov. 2023"
-              image="https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYRsy9bb1pEaqmBo6NWoC8GNYWEvrt5W2SRqctzYxPeJM6hJ8VF5cR1ZAO-_0uhvHnmBq2ZgWTprg-vYAMbTC0CVRwo0wQ"
+              image="https://i.postimg.cc/xTnpxF54/16-nov-2023.jpg"
             />
 
             <Illustration
               date="Aug. 2023"
-              image="https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYQWWzTWsqpOUlvnTpCJ0r-XrBiLTL-GvUJ3sm_BGmTbV_hDemVu5Y4qq-2p5xhrB41I-LDWHhHze3JCf_qGmw_tWmvBqA"
+              image="https://i.postimg.cc/c1Q5cNR3/20-agosto-2023.png"
             />
 
             <Illustration
               date="Aug. 2023"
-              image="https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYSgIPZOL8h4DKs41dtw-IWHQT66zfJd2pCgTXdmPu6W1KEPT7kwjx_3dPdmPecVZT9ORuls_QPKYxfgKEIJD5pVmpK2"
+              image="https://i.postimg.cc/N0vV1yV5/30-agosto-2023.png"
             />
 
             <Illustration
               date="Aug. 2023"
-              image="https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYQtKYRzc85yDJxluTiZwc9_ugdxAuy0CNDNMs6pqf-nwo6YAXliDztTuhzBYseN9KmsfQSgeGc7TUZLcg_hhFdAwT18sw"
+              image="https://i.postimg.cc/Pqg2m6NK/24-agosto-2023.png"
             />
 
             <Illustration
               date="Aug. 2023"
-              image="https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYSvedeBHGTdEzwnodLa1mR3XaV8ESIYUuf9DYVoEAiONJKouWy3mbJA-hyU8BQXXYLAZ_i0M8CzNzhkfKnwPf_sXpto"
+              image="https://i.postimg.cc/PrQ9dkNH/2-agosto-2023.png"
             />
 
             <Illustration
               date="Jun. 2023"
-              image="https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYTydT02kD-eimr6wZrg-ds7NhgSfryHHkxgev0nFKRWlesDbowi5x65DDNJURJO7adLOSwK74SyRwVQ3HP5GByZkRn4HA"
+              image="https://i.postimg.cc/hGwkzb91/10-junho-2023.png"
             />
 
             <Illustration
               date="Jun. 2023"
-              image="https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYSKsPQrYc33zAEKd0E7mNhRDQXUl2aNlNkGbNqtTEB9TVy75uZ_6xaCvcbLELkNfWO--MC0tn8Ml2T8H27H1axwft5O7A"
+              image="https://i.postimg.cc/3rGtZrXG/13-junho-2023.png"
             />
 
             <Illustration
               date="Jun. 2023"
-              image="https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYQz45CJfDubnfWaVo-FCvGnojwA3eThWOlqucwu3zI_m5MhD3wb5VWuzvCra6GyhZgIdtnch0uBxM5Y9lqxyJsGYkEm"
+              image="https://i.postimg.cc/jj9DgbHw/26-junho-2023.jpg"
             />
 
             <Illustration
               date="Jun. 2023"
-              image="https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYTkisWy3pzt0ejHXI8XmeMrj_tT1dP7NfDBl7N_VUp5V9Ystayib3zTSN_O8kpTSXUIf1iyT3uBJUzBVwhif7v4TWpaxQ"
+              image="https://i.postimg.cc/3NCSrg81/7-junho-2023.png"
             />
 
             <Illustration
               date="May. 2023"
-              image="https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYTEXg01iyNyi_1-PosPrO473KD2WS_WpZs8iia7hVW9mfT5qFBR34j1T2bTGIEkW61VrDPSS7mwBOm6dtzSgEu5LmAHww"
+              image="https://i.postimg.cc/XYfx6sHY/4-maio-2023.jpg"
             />
 
             <Illustration
               date="Apr. 2023"
-              image="https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYSfK9AM9-9ky55rxUcTwdKNcM1s-KvTFFZZKw6TfeL3_PQzPd9V3x83AcfCvQxwrA9eFy72dYW3OpEsiY6yhpA51tBogA"
+              image="https://i.postimg.cc/6QymV2c6/11-abril-2023.png"
             />
 
             <Illustration
               date="Dec. 2022"
-              image="https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYRePfUTpgglD7uRrSEwMlk3HOVgcy6Z23ggSqaqDl8RHmYWyw6IQWB7hw9xPj9WIQXhZf1_5K2VF49TC6lc3PuhbEkjLA"
+              image="https://i.postimg.cc/CM2cGBnS/14-dez-2022.png"
             />
 
             <Illustration
               date="Dec. 2022"
-              image="https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYTVKI2euy-w0Vwsz1qUKWwBS7dPl-3yEkOY98kXXJStsVZeMUvU500I8Br3N9ZETmDgiAiPkzvFE0x-jB4OrgLhpPoD"
+              image="https://i.postimg.cc/J7H2d5LG/17-dez-2022.png"
             />
 
             <Illustration
               date="Dec. 2022"
-              image="https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYThVrDOkid_6cZiPaT3DqgPWHfS94Bum2OfWv_WMgzvdhYuTuT6nP8Ok9DCjM5XP7yRHlCAP34pCc3poVXb1BRWkVtLSg"
+              image="https://i.postimg.cc/L5yKDHPG/22-dez-2022.png"
             />
 
             <Illustration
               date="Nov. 2022"
-              image="https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYTDffBlVClimEgosheo6aD3FSiqThvW1fGtw9o1wNxrAKd_RPMBY3zzo4w1C67Sythu5pq2_4a-x6ASmmH9bqceSwPilw"
+              image="https://i.postimg.cc/QxPvdYJg/18-nov-2022.png"
             />
 
             <Illustration
               date="Nov. 2022"
-              image="https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYS_iiQWK5b3O6Lesea44yduyGySu6r8bLIroQ-vVb3eFVAv4AeKfExUUMcRp1LKHd4TNIQfhLQYrjyi_aAmUgedOKX0lw"
+              image="https://i.postimg.cc/JnmfWyc9/23-novembro-2022.png"
             />
 
             <Illustration
               date="Oct. 2022"
-              image="https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYQOgHb1gSqe2UD_S49-a-KYZQgtlscxhXHUEd8fPAC0wcPAz_v8kI1D35gZxTNmVjw3fMt3dMV2dzVM2wgK_ZGz6eVOTA"
+              image="https://i.postimg.cc/GmX8DY9j/22-out-2022.jpg"
             />
 
             <Illustration
               date="Apr. 2022"
-              image="https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYRxxq8BQ5o99ERAGfDgW52OmIO_U07-q0wtehgu9ciKbo4SY9XO9sdg_I0Q_gNor6aY4ogqKpTInrwkTbaC1Y-XbHn8"
+              image="https://i.postimg.cc/9MJn5Lf7/10-abril-2022.jpg"
             />
 
             <Illustration
               date="Apr. 2022"
-              image="https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYT89Fp-LXxGs0ULE434hwnkc7Mnq5mlDiC6LpPb2t2n-gHbvpJFT3ukPpyT3I9PmxLjpEYJUGWgKkGlSMoWcxLugcqkGQ"
+              image="https://i.postimg.cc/PfSBB5LR/28-abril-2022.png"
             />
 
             <Illustration
               date="Jan. 2022"
-              image="https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYRoEiA5JXtNh_2WLpdyLvmJZq9p6bpSnfOpqnxTK2ex6Vg8pJggnq4ksrDy9aAmEruTfvvPNSy9-p77OD7urKp4Pqau"
+              image="https://i.postimg.cc/sgdKLkJL/6-Jan-2022.jpg"
             />
 
             <Illustration
               date="Jan. 2022"
-              image="https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYReHM7vDkQQwUC2J09NesNH8WM2gdopUc4VP9CYjHbNGQ_OS9Oo8ABa6l3ZcxhRP1UkmlNgxnNP1umHfsnukA61axdupQ"
+              image="https://i.postimg.cc/1X726vhQ/17-Jan-2022.png"
             />
 
             <Illustration
               date="Dec. 2021"
-              image="https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYQ8SSta64o5yDGrFPredd_jxSfCWDYSRbwpW4oVa39U3wvCz4PAT9zZ9uBTc6grf7vHyKbEPdk9htPmRn7ayJVtX0gjpQ"
+              image="https://i.postimg.cc/DzmWPZX3/19-dez-2021.jpg"
             />
 
             <Illustration
               date="Dec. 2021"
-              image="https://lh3.googleusercontent.com/u/0/drive-viewer/AEYmBYTOIvBRdciIB_3Dy4YC-b9D-zRDae4FCS0qkNu-rftQUfUZwXM_puosu2AS0ncVgb5sf3JrMzLw79KimHirMQnBd8EB"
+              image="https://i.postimg.cc/ZnnDT69D/9-dez-2021.jpg"
             />
           </IllustrationsOrganization>
         </section>
